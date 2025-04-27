c38cc7aedc2720774f936868e43f68cbe4b079de
MrThonChan <98427928+MrThonChan@users.noreply.github.com>
2025-04-27 22:30:24 +0200

    added comments to input handler
    added comments to input handler (functions need to be modified in order to work with the new objects correctly)

--- core/input_handler.py
+++ core/input_handler.py
@@ -1,13 +1,43 @@
+"""
+input_handler.py
+
+Library of functions that handle the possible actions of the user.
+
+Features include:
+- Define if left click is pressed
+- (to modify) Detect if a left click is on/in a circle (currently --> must be in an object later on)
+- Vector application during paused game script
+
+Author: Maxime Noudelberg
+Last Updated: April 2025
+Python Version: 3.12.10
+Dependencies: pygame, pygame.math, utils.vector_utils
+"""
+
 import pygame 
 from pygame.math import Vector2
 from utils.vector_utils import * 
 pygame.init()
 
 def GetMouseInput(event) :
+    """
+    Return 1/0 if left click pressed/not
+
+    Parameters:
+    event (<class 'pygame.event.Event'>) : defined class that detects events
+    """
     return event.type == pygame.MOUSEBUTTONDOWN #autorise qu'un seul clic gauche jusqu'au relachement de la touche  --> on peut pas maintenir la touche pour faire clic gauche continuellement
 
 #verifies if the mouse is in the radius of a clicked circle
 def is_point_in_circle(point, circle_center, radius):
+    """
+    Return 1/0 if mouse position is between the center of a circle and its radius/not
+
+    Parameters:
+    point (tuple of integers): The position of the mouse
+    circle_center (array of integers (?)) : Actual coordinates of the center of our circle
+    radius (integer) : Radius of this same circle
+    """
     dx = point[0] - circle_center[0]
     dy = point[1] - circle_center[1]
     return dx**2 + dy**2 <= radius**2
@@ -20,6 +50,23 @@ def vector_application(
     vector1_coords, vector2_coords, 
     vector1_angle, vector2_angle
 ):
+    
+    """
+    Handles the application of a Vector2 on circle (modify for an object later on) : can apply up to 2 vectors at the same time (1 per object)
+    Updates the variables clicked_object, vector_applied1, vector_applied2, vector1_coords, vector2_coords, vector1_angle, vector2_angle, mouse_position
+    depending on the actions of the user 
+
+    Parameters:
+    clicked_object (object from the objects.json (?)) : The object manipulated bu the user
+    vector_applied1 (boolean) : Defines if the user has applied a vector to test_object during game_state = "paused" 
+    vector_applied2 (boolean) : Defines if the user has applied a vector to second_object during game_state = "paused" 
+    vector1_coords (Vector2) : Coordinates of the vector (if) applied to test_object
+    vector2_coords (Vector2) : Coordinates of the vector (if) applied to second_object
+    vector1_angle (integer) : angle between center of circle (test_object) and position of the mouse, in the normal plane (x>0 to the right, y>0 upwards) 
+    vector2_angle (integer) : angle between center of circle (second_object) and position of the mouse, in the normal plane (x>0 to the right, y>0 upwards) 
+    mouse_position (tuple of integers): The position of the mouse
+    """
+    
     mouse_position = pygame.mouse.get_pos()
 
     if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
@@ -62,59 +109,3 @@ def vector_application(
         clicked_object = None
 
     return clicked_object, vector_applied1, vector_applied2, vector1_coords, vector2_coords, vector1_angle, vector2_angle, mouse_position
-
-
-
-
-
-
-
-# WIDTH = 1000
-# HEIGHT = 500
-# fps = 60
-# timer = pygame.time.Clock()
-# screen = pygame.display.set_mode([WIDTH,HEIGHT])
-
-
-# ghost_image = pygame.image.load("assets/sprites/sprite_ghost.png")
-# ghost_image = pygame.transform.scale(ghost_image, (100, 100))
-
-# is_possessing = False
-# click_ready = True
-
-
-# run = True
-# while run :
-#     screen.fill('white')
-#     timer.tick(fps)
-#     pygame.mouse.set_visible(False) #make the mouse cursor invisible
-#     mouse_position = pygame.mouse.get_pos() #obtain the position of the mouse in real time and store it in an array
-#     mouse_x = mouse_position[0] #can be deleted if not necessary for the calculations of the forces
-#     mouse_y = mouse_position[1] #can be deleted if not necessary for the calculations of the forces
-    
-    
-#     # not useful at the moment (--> use of an event for possession)
-#     left_clicked = pygame.mouse.get_pressed()[0] #return a boolean : if left click is pressed or not (maintaining keep returning True)  
-    
-    
-    
-    
-#     if is_possessing is False:
-#         screen.blit(ghost_image, (mouse_x - ghost_image.get_width() // 2, mouse_y - ghost_image.get_height() // 2))  #display the ghost sprite such that the mouse is the center of the sprite
-    
-    
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             run = False
-            
-            
-#         # At the pressure of left click, possession state changes
-#         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and click_ready: # "DOWN" = button pressed ; "button == 1" = left click is pressed
-#             is_possessing = not is_possessing  # possession state change
-#             click_ready = False  # if left click is maintained, the possession state cannot be changed until the release of it
-
-#         if event.type == pygame.MOUSEBUTTONUP and event.button == 1: # "UP" = release of the button
-#             click_ready = True  # once left click is released, give the permission to change the state of possession again
-    
-#     pygame.display.flip()
-# pygame.quit()
